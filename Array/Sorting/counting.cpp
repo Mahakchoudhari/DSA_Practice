4325b170d84131fe2e3366e3960f7fbeb83f6a0b
Mahakchoudhari <mehakchoudhari786@gmail.com>
2026-08-06 23:34:02 +0530

    Adding counting sort in sorting tech

---
+++ Array/Sorting/counting.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+void  countingSort(int arr[]  ,int size){
+    int max = arr[0];
+    for(int i=1 ;i<size;i++){
+        if(arr[i]>max){
+              max=arr[i];
+        }
+    }
+    
+    vector<int> count(max+1);
+    for(int i=0 ;i<size;i++){
+         count[arr[i]]++;
+
+     }
+       
+
+    int index = 0;
+
+    for (int i = 0; i <= max; i++)
+    {
+        while (count[i] > 0)
+        {
+            arr[index++] = i;
+            count[i]--;
+        }
+    }
+
+
+}
+int main(){
+    int arr[] = {4,2,2,8,3,3,1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    countingSort(arr , size);
+
+    for(int x : arr)
+        cout << x << " ";
+}
