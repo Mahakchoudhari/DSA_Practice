c4ad0a3196ed3a99ce16705bf889e8bab0c2a59a
Mahakchoudhari <mehakchoudhari786@gmail.com>
2026-08-06 23:49:26 +0530

    Modifying size of vector in counting sort

--- Array/Sorting/counting.cpp
+++ Array/Sorting/counting.cpp
@@ -9,7 +9,7 @@ void  countingSort(int arr[]  ,int size){
         }
     }
     
-    vector<int> count(max+1);
+    vector<int> count(max+1 , 0);
     for(int i=0 ;i<size;i++){
          count[arr[i]]++;
 
